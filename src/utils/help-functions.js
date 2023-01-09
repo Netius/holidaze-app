651bc50aee81ff89af38857bdaaa681a0fc78f8f
NetiusSP <fabiomoraisnetosp@hotmail.com>
2023-01-09 17:16:33 +0100

    Created help function to added image

---
+++ src/utils/help-functions.js
@@ -0,0 +1,30 @@
+import { UPLOAD_URL } from "../constants/api";
+
+export default async function addImageHotel(formFile, id, token) {
+    if (!formFile) return;
+
+    var formData = new FormData();
+
+    formData.append("files", formFile.files[0]);
+    formData.append('refId', id);
+    formData.append('ref', "hotels");
+    formData.append('field', "image");
+
+    const options = {
+        method: "POST",
+        body: formData,
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    try {
+        const response = await fetch(UPLOAD_URL, options);
+        if (!response.ok) {
+            throw "Error: Image to product not uploaded.";
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
