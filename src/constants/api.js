c1f270a6144c9a4aaea79811dd71a019de6f7e69
NetiusSP <fabiomoraisnetosp@hotmail.com>
2023-01-09 17:17:11 +0100

    Added some more urls

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,3 +2,4 @@
 export const BASE_URL = "http://localhost:1337";
 export const AUTH_URL = BASE_URL + "/auth/local";
 export const HOTELS_URL = BASE_URL + "/hotels/";
+export const UPLOAD_URL = BASE_URL + "/upload/";
