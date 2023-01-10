02018869404cf5ffe26fd2591e4c419116c69d96
NetiusSP <fabiomoraisnetosp@hotmail.com>
2023-01-10 14:24:57 +0100

    Added Cards and error handling

---
+++ src/components/hotels/hotels-card.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { HOTELS_URL } from "../../constants/api";
+import FormError from "../../common/form-error";
+
+function HotelsCard() {
+	const [hotels, setHotels] = useState([]);
+	const [error, setError] = useState(null);
+
+	const getHotels = async () => {
+		axios
+			.get(HOTELS_URL)
+			.then((response) => {
+				setHotels(response.data);
+			})
+			.catch((error) => {
+				setError(error);
+				console.error(error);
+			});
+	};
+
+	useEffect(() => {
+		getHotels();
+	}, []);
+
+	return (
+		<>
+			{hotels &&
+				hotels.map((hotel, index) => {
+					return (
+						<>
+							<div className="card my-4">
+								<div className="row g-0">
+									<div className="col-md-4">
+										<img src={hotel.image[0]?.url} className="img-fluid rounded-start" alt="..." />
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{hotel.title}</h5>
+											<p className="card-text">{hotel.description}</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</>
+					);
+				})}
+			{error && <FormError>{error}</FormError>}
+		</>
+	);
+}
+
+export default HotelsCard;
--- src/components/hotels/hotels-page.jsx
+++ src/components/hotels/hotels-page.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Heading from "../../common/Heading";
+import SubHeading from "../../common/subheading";
+import HotelsCard from "./hotels-card";
 
 function HotelsPage() {
 	return (
-		<div className="container-fluid text-center">
+		<div className="container">
 			<Heading header={"Hotels"} />
+			<SubHeading header={"Check out our premium hotels."} />
+
+			<HotelsCard />
 		</div>
 	);
 }
