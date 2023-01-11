2e65d2772bbbf9a3eb635686ae784b1400d3f73a
NetiusSP <fabiomoraisnetosp@hotmail.com>
2023-01-11 10:43:56 +0100

    Fixed better error message

--- src/components/error/error-page.jsx
+++ src/components/error/error-page.jsx
@@ -1,10 +1,12 @@
 import Heading from "../../common/Heading";
+import SubHeading from "../../common/subheading";
 
 export default function ErrorPage() {
 	return (
-		<div id="error-page" className="text-center">
-			<Heading header={"Oops!"} />
-			<p>Sorry, an unexpected error has occurred.</p>
+		<div id="error-page" className="text-center mt-5">
+			<i className="far fa-frown display-1"></i>
+			<Heading header={"404"} />
+			<SubHeading header={"Page not found"} />
 		</div>
 	);
 }
