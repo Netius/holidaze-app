4ab88899ad9ce4d47db0ed7e0377ab23126a90bb
NetiusSP <fabiomoraisnetosp@hotmail.com>
2023-01-13 15:04:39 +0100

    Fixed contacts

--- src/components/contact/contact-form.jsx
+++ src/components/contact/contact-form.jsx
@@ -18,66 +18,64 @@ function ContactForm() {
 	}
 
 	return (
-		<div className="container">
-			<div className="row ">
-				<div className="col-md-6">
-					<Heading header={"Contact us"} />
-					<SubHeading header={"Any questions or sugestions just let us know"} />
-					<form onSubmit={(e) => handleSubmit(e)}>
-						<div className="mb-3 mt-4">
-							<label htmlFor="contact-name" className="form-label">
-								Fullname
-							</label>
-							<input
-								id="contact-name"
-								name="contact-name"
-								type="text"
-								value={fullName}
-								className="form-control"
-								onChange={(e) => setFullName(e.target.value)}
-								required
-							/>
-						</div>
-						<div className="mb-3">
-							<label htmlFor="contact-email" className="form-label">
-								Email
-							</label>
-							<input
-								id="contact-email"
-								name="contact-email"
-								type="email"
-								value={email}
-								className="form-control"
-								onChange={(e) => setEmail(e.target.value)}
-								required
-							/>
-						</div>
-						<div className="mb-3">
-							<label htmlFor="contact-message" className="form-label">
-								Message
-							</label>
-							<textarea
-								id="contact-message"
-								name="contact-message"
-								className="form-control"
-								value={message}
-								rows={6}
-								cols={50}
-								minLength={20}
-								onChange={(e) => setMessage(e.target.value)}
-								required
-							/>
-						</div>
-						<button type="submit" className="btn btn-success">
-							Send
-						</button>
-						{saved && <div className=" alert alert-success my-3">Thank you! Message sendt.</div>}
-					</form>
-				</div>
+		<div className="row ">
+			<div className="col-md-6">
+				<Heading header={"Contact us"} />
+				<SubHeading header={"Any questions or sugestions just let us know"} />
+				<form onSubmit={(e) => handleSubmit(e)}>
+					<div className="mb-3 mt-4">
+						<label htmlFor="contact-name" className="form-label">
+							Fullname
+						</label>
+						<input
+							id="contact-name"
+							name="contact-name"
+							type="text"
+							value={fullName}
+							className="form-control"
+							onChange={(e) => setFullName(e.target.value)}
+							required
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="contact-email" className="form-label">
+							Email
+						</label>
+						<input
+							id="contact-email"
+							name="contact-email"
+							type="email"
+							value={email}
+							className="form-control"
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="contact-message" className="form-label">
+							Message
+						</label>
+						<textarea
+							id="contact-message"
+							name="contact-message"
+							className="form-control"
+							value={message}
+							rows={6}
+							cols={50}
+							minLength={20}
+							onChange={(e) => setMessage(e.target.value)}
+							required
+						/>
+					</div>
+					<button type="submit" className="btn btn-success">
+						Send
+					</button>
+					{saved && <div className=" alert alert-success my-3">Thank you! Message sendt.</div>}
+				</form>
+			</div>
 
-				<div className="col-md-6 py-5">
-					<div className="contact__image" style={{ backgroundImage: `url(${ContactImage})` }}></div>
-				</div>
+			<div className="col-md-6 py-5">
+				<div className="contact__image" style={{ backgroundImage: `url(${ContactImage})` }}></div>
 			</div>
 		</div>
 	);
