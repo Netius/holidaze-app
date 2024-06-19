6a027381b6b8b8112c7b93a1eb8cf45de88279ff
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2024-06-19 09:44:31 +0200

    New api url changed from API Strapi

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,8 @@
 
-// export const BASE_URL = "http://localhost:1337";
-export const BASE_URL = "https://safe-retreat-07204.herokuapp.com";
-export const AUTH_URL = BASE_URL + "/auth/local";
-export const HOTELS_URL = BASE_URL + "/hotels/";
-export const HOTELS_MESSAGES = BASE_URL + "/hotels-messages/";
+export const BASE_URL = "http://localhost:1337";
+// export const BASE_URL = "https://safe-retreat-07204.herokuapp.com";
+export const AUTH_URL = BASE_URL + "/api/auth/local";
+export const HOTELS_URL = BASE_URL + "/api/hotels/";
+export const HOTELS_MESSAGES = BASE_URL + "/api/hotels-messages/";
 export const UPLOAD_URL = BASE_URL + "/upload/";
+export const POPULATE_ALL = "?populate=*"
