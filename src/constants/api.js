24892332daafba906077a047457a315ce0f1e875
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2024-06-20 08:00:20 +0200

    Fixed api url to upload

--- src/constants/api.js
+++ src/constants/api.js
@@ -4,5 +4,5 @@ export const BASE_URL = "http://localhost:1337";
 export const AUTH_URL = BASE_URL + "/api/auth/local";
 export const HOTELS_URL = BASE_URL + "/api/hotels/";
 export const HOTELS_MESSAGES = BASE_URL + "/api/hotels-messages/";
-export const UPLOAD_URL = BASE_URL + "/upload/";
+export const UPLOAD_URL = BASE_URL + "/api/upload/";
 export const POPULATE_ALL = "?populate=*"
